953ff222d7dbb78845cff80d87c0c028a74ed6a1
jyothisjkj <93486900+jyothisjkj@users.noreply.github.com>
2022-07-06 17:44:57 +0530

    Add files via upload

---
+++ Jyothis/9.12.21/Activity_convert_video_to _grayscale.py
@@ -0,0 +1,16 @@
+# Convert video into Grayscale
+import cv2
+cap = cv2.VideoCapture('C:\\Users\\rahul.nair\\Desktop\\vdo.avi')
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter('C:\\Users\\rahul.nair\\Desktop\\myvdo.avi',fourcc,30,(720,528))
+while cap.isOpened():
+    ret,img=cap.read()
+    if ret==True:
+        gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        cv2.imshow('Output',gray_img)
+        out.write(gray_img)
+    if cv2.waitKey(1)==ord('q'):
+        break
+cap.release()
+out.release()
+cv2.destroyAllWindows()
